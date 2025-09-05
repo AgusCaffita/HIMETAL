5bf4041ca086782c97e61a50b63a38dcd6712058
Agus <agustinacaffaratti4@gmail.com>
2025-09-05 11:12:12 -0300

    mas cambios boludos

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -10,7 +10,7 @@ const Home = () => {
                 <h2>HIMetal</h2>
             </div>
             <p className="bg-blue-300">Estas en la pagina principal </p>
-            <button onClick={() => navigate("/invoicer")} className="m-4 hover:text-pink-600">Hacer factura</button>
+            <button onClick={() => navigate("/invoicer")} className="m-4 hover:text-pink-600">Hacer facturaaa</button>
             <button onClick={() => navigate("/articulos")} className="m-4 hover:text-pink-600">Ver articulos</button>
             <button onClick={() => navigate("/piezas")} className="m-4 hover:text-pink-600">Ver piezas</button>
             <button onClick={() => navigate("/user")} className="m-4 hover:text-pink-600">Ver perfil</button>
